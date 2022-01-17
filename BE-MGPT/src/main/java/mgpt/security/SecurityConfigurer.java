e5e06be75ffffd58d988e12226b16600444d8d38
ahuuwx <huuntse130770@fpt.edu.vn>
2022-01-17 08:38:48 +0700

    [HuuNT] fix jwt in constant secret key

--- BE-MGPT/src/main/java/mgpt/security/SecurityConfigurer.java
+++ BE-MGPT/src/main/java/mgpt/security/SecurityConfigurer.java
@@ -42,7 +42,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers(Constant.URL_LOGIN, Constant.URL_GET_PROFILE)
+                .antMatchers(Constant.URL_LOGIN, Constant.URL_GET_PROFILE,"/get-profile")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
--- BE-MGPT/src/main/java/mgpt/util/Constant.java
+++ BE-MGPT/src/main/java/mgpt/util/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
     /**
      * JWT
      */
-    public static final String SECRET_KEY = "MPGT";
+    public static final String SECRET_KEY = "MPGT_FA21#MPGT";
     public static final String STRING_AUTHORIZATION = "Authorization";
     public static final String URL_LOGIN = "/login";
     public static final String URL_GET_PROFILE = "/get-profile**";
